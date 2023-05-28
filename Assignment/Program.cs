e9608c6c961432d278cca157a474fd9170c3147c
Arnab Chattopadhyay <59999832+akbc1221@users.noreply.github.com>
2023-05-29 03:03:48 +0530

    Add files via upload

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main()
         {
-            System.Console.WriteLine("Hello World");
+            // Basic Testing
+            System.Console.WriteLine("Testing TreasureChest.cs ...");
+            TreasureChest chest = new(TreasureChest.State.Closed);
+
+            TreasureChest.State state1 = chest.Manipulate(TreasureChest.Action.Open);
+            Console.WriteLine("Current state is: " + state1);
+
+            TreasureChest.State state2 = chest.Manipulate(TreasureChest.Action.Lock);
+            Console.WriteLine("Current state is: " + state2);
+
+            TreasureChest.State state3 = chest.Manipulate(TreasureChest.Action.Close);
+            Console.WriteLine("Current state is: " + state3);
+
+            TreasureChest.State state4 = chest.Manipulate(TreasureChest.Action.Lock);
+            Console.WriteLine("Current state is: " + state4);
         }
     }
 }
